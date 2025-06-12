42e7e50c7675928998e1f1fd75477c55631c01de
Eglijohn <eglijohn@posteo.ch>
2025-06-12 19:08:31 +0200

    upidati

--- script.js
+++ script.js
@@ -22,3 +22,9 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+
+document.addEventListener('mousemove', function(e) {
+    const dot = document.getElementById('cursor-dot');
+    dot.style.left = e.clientX + 'px';
+    dot.style.top = e.clientY + 'px';
+});
